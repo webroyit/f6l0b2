22c317c72dfd7ff535a59cc5ba450b4984cb72d8
royweb <xytank@gmail.com>
2020-06-07 15:29:39 -0400

    Set up variables

--- Assets/Scripts/TapController.cs
+++ Assets/Scripts/TapController.cs
@@ -2,7 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Create Rigidbody2D component
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class TapController : MonoBehaviour
 {
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    Rigidbody2D rigidbody;
+
+    // Rotation
+    Quaternion downRotation;
+    Quaternion forwardRotation;
     
 }
