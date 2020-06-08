df80e01bf75e7fea5accbee03b988faac7c59188
royweb <xytank@gmail.com>
2020-06-08 13:41:08 -0400

    Set up variables

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;       // For text
 
 public class GameManager : MonoBehaviour
 {
-    
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countdownPage;
+    public Text scoreText;
+
+    enum PageState
+    {
+        None,
+        Start,
+        GameOver,
+        Countdown
+    }
+
+    int score = 0;
+    bool gameOver = false;
+
+    // To prevent other script from changing this value
+    public bool GameOver { get { return gameOver; }}
 }
