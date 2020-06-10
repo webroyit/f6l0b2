3fbd6820d62bdc7f56499fc20fc396bb62660c5e
royweb <xytank@gmail.com>
2020-06-10 15:28:03 -0400

    Add a class called PoolObject

--- Assets/Scripts/Parallaxer.cs
+++ Assets/Scripts/Parallaxer.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Parallaxer : MonoBehaviour
 {
-    
+    // Keeps track of clouds and pipes
+    class PoolObject
+    {
+        public Transform transform;
+        public bool inUse;
+        public PoolObject(Transform t) { transform = t; }
+        
+        public void Use() { inUse = true; }
+
+        public void Dispose() { inUse = false; }
+    }
 }
