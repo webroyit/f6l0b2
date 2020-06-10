c06820cd6e0ab8db3df5e4696350f29a8b34680e
royweb <xytank@gmail.com>
2020-06-10 14:45:35 -0400

    Get the high score from local device

--- Assets/Scripts/HighscoreText.cs
+++ Assets/Scripts/HighscoreText.cs
@@ -1,8 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;       // For text
 
+// Required Text
+[RequireComponent(typeof(Text))]
 public class HighscoreText : MonoBehaviour
 {
-    
+    Text highscore;
+
+    void OnEnable()
+    {
+        highscore = GetComponent<Text>();
+
+        // Get the highscore from local device
+        highscore.text = PlayerPrefs.GetInt("HighScore").ToString();
+    }
 }
